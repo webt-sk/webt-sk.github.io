5a6b69828c799afc65fc2d5513a3d77d52083fa5
Marcel Malych <marcelmalych5@gmail.com>
2022-04-24 16:52:17 +0200

    dark theme working

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import Navbar from "../components/Navbar";
 
-export default function Home() {
+export default function Home(props) {
   return (
     <>
       <Navbar />
-      <h1 className="underline font-bold text-3xl text-stone-600">
+      <h1 className="underline font-bold text-3xl text-stone-600 dark:text-cyan-200">
         Hello world
       </h1>
     </>
