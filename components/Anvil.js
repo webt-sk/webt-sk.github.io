28224c15426e4f19c74fab6c77cf4316281f16ec
Marcel Malych <marcelmalych5@gmail.com>
2022-05-04 20:25:56 +0200

    added orbital control

--- components/Anvil.js
+++ components/Anvil.js
@@ -1,10 +1,12 @@
 import { useEffect, useRef } from "react";
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 import { loadGLTFModel } from "../lib/model";
 
 export default function Anvil() {
   const div = useRef();
+  const target = new THREE.Vector3(0, 0, 0);
 
   useEffect(() => {
     const scene = new THREE.Scene();
@@ -19,24 +21,38 @@ export default function Anvil() {
     renderer.shadowMap.enabled = true;
 
     camera.position.z = 3;
+    camera.position.y = 0.8;
 
-    // const light = new THREE.PointLight(0xffffff, 1, 100, 2);
-    // light.power = 50;
-    // light.position.set(-10, 10, 19);
-    // scene.add(light);
+    const light = new THREE.PointLight(0xffffff, 1, 100, 2);
+    light.power = 48;
+    light.position.set(-10, 10, 19);
+    scene.add(light);
 
-    const directionalLight = new THREE.DirectionalLight(0xffffff, 3);
-    directionalLight.position.set(-10, 10, 19);
-    directionalLight.castShadow = true;
-    scene.add(directionalLight);
+    // const ambientLight = new THREE.AmbientLight(0xffffff, 0.8);
+    // scene.add(ambientLight);
+
+    // const directionalLight = new THREE.DirectionalLight(0xffffff, 3);
+    // directionalLight.position.set(-10, 10, 19);
+    // directionalLight.castShadow = true;
+    // scene.add(directionalLight);
 
     loadGLTFModel(scene, "/anvil.glb", {
       receiveShadow: false,
       castShadow: true,
     }).then(() => {
       div.current.appendChild(renderer.domElement);
-      renderer.render(scene, camera);
+      animate();
     });
+
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.autoRotate = true;
+    controls.target = target;
+
+    function animate() {
+      requestAnimationFrame(animate);
+      controls.update();
+      renderer.render(scene, camera);
+    }
   });
 
   return <div ref={div}></div>;
