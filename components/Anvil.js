6e5a4a71f18e6849f5d7c41b287931518365e286
Marcel Malych <marcelmalych5@gmail.com>
2022-05-04 13:20:13 +0200

    3d rendering working finally - needed to be async

--- components/Anvil.js
+++ components/Anvil.js
@@ -1,45 +1,43 @@
 import { useEffect, useRef } from "react";
 import * as THREE from "three";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+
+import { loadGLTFModel } from "../lib/model";
 
 export default function Anvil() {
-  const scene = new THREE.Scene();
-  const refContainer = useRef();
-  const loader = new GLTFLoader();
+  const div = useRef();
 
   useEffect(() => {
-    const ambientLight = new THREE.AmbientLight(0xcccccc, 5);
-    scene.add(ambientLight);
+    const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(
-      75,
+      35,
       window.innerWidth / window.innerHeight,
       0.1,
       1000
     );
-    camera.position.set(3, 10, 1);
-
-    const renderer = new THREE.WebGLRenderer();
-
+    const renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
     renderer.setSize(window.innerWidth, window.innerHeight);
-    refContainer.current.appendChild(renderer.domElement);
+    renderer.shadowMap.enabled = true;
 
-    loader.load(
-      "/anvil.glb",
-      (glb) => {
-        const obj = glb.scene;
-        obj.position.y = 0;
-        obj.position.x = 0;
-        obj.scale.set(500, 500, 500);
-        scene.add(obj);
-      },
-      undefined,
-      (error) => {
-        console.error(error);
-      }
-    );
+    camera.position.z = 3;
 
-    renderer.render(scene, camera);
-  }, []);
+    // const light = new THREE.PointLight(0xffffff, 1, 100, 2);
+    // light.power = 50;
+    // light.position.set(-10, 10, 19);
+    // scene.add(light);
 
-  return <div ref={refContainer}></div>;
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 3);
+    directionalLight.position.set(-10, 10, 19);
+    directionalLight.castShadow = true;
+    scene.add(directionalLight);
+
+    loadGLTFModel(scene, "/anvil.glb", {
+      receiveShadow: false,
+      castShadow: true,
+    }).then(() => {
+      div.current.appendChild(renderer.domElement);
+      renderer.render(scene, camera);
+    });
+  });
+
+  return <div ref={div}></div>;
 }
