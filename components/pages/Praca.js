4c92dcc57c969389e9368fb4dc2a14f93208f7c8
Marcel Malych <marcelmalych5@gmail.com>
2022-05-12 11:48:13 +0200

    prace edit, prace page

--- components/pages/Praca.js
+++ components/pages/Praca.js
@@ -3,7 +3,18 @@ import Layout from "../Layout";
 export default function Praca(props) {
   return (
     <Layout otherLang={props.otherLang} otherPage={props.otherPage}>
-      <p className="text-white">{props.f.nazovPopis}</p>
+      <div
+        className="w-12 h-1/2"
+        style={{
+          borderBottomLeftRadius: "100px",
+          backgroundColor: props.color,
+        }}
+      />
+      <div>
+        <h1 className="dark:text-white">{props.nazov}</h1>
+        <p className="dark:text-white">{props.f.popis}</p>
+      </div>
+      <img src={props.image} alt={props.f.imageAlt} />
     </Layout>
   );
 }
