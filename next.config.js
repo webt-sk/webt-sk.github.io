5fa4820e8106edff58719f08f7e39b4bc475a7e3
Marcel Malych <marcelmalych5@gmail.com>
2022-05-20 09:42:18 +0200

    fixing deploy

--- next.config.js
+++ next.config.js
@@ -3,8 +3,6 @@ const isProd = process.env.NODE_ENV === "production";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  basePath: isProd ? "https://webt.sk" : "",
-  assetPrefix: isProd ? "https://webt.sk" : "",
 };
 
 module.exports = nextConfig;
